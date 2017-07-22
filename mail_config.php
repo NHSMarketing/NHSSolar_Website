e66e0ca60db31f911b345cc8bc3ccdc6b5c37b9a
dayperetti <30347993+dayperetti@users.noreply.github.com>
2017-07-22 10:37:41 -0300

    Add files via upload

---
+++ mail_config.php
@@ -0,0 +1,12 @@
+<?php
+
+	$host = 'smtp.nhs.com.br';
+	$port = 25;
+	$email_falecom = 'falecom@nhs.com';
+	$senha_falecom = 'falecomnhs';
+	$from_falecom = $email_falecom;
+	$to_falecom = 'falecom@nhs.com.br';
+	$email_marketing = 'marketing@nhs.com';
+	$senha_marketing = 'marketingnhs';
+	$from_marketing = $email_marketing;
+	$to_marketing = 'marketing@nhs.com.br';
